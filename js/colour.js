69a08811aefa2b96d27e81f55fd6668dd7f644ce
Barak <barakadax@gmail.com>
2020-08-08 21:15:58 +0300

    change for indicative value

--- js/colour.js
+++ js/colour.js
@@ -10,7 +10,7 @@
 　　  /  ╰    ╯  /
 code by Barakadax*/
 'use strict';
-let correct = "";
+let correctCircle = "";
 let colours = [0,0,0];              //Create RGB code needs 3 values
 let circlesRGBValues = [0,0,0];     //3 circles
 let circles = [document.getElementById("first_blob"), document.getElementById("second_blob"), document.getElementById("third_blob")];
@@ -42,8 +42,8 @@ function initialize() {
         circles[run].style.background = circlesRGBValues[run] = 'rgb(' + colours[run] + ',' + colours[(run + 1) % circlesRGBValues.length] + ',' + colours[(run + 2) % circlesRGBValues.length] + ')';
         circles[run].style.boxShadow = "5px 5px 105px 5px #ff8c00";
     }
-    correct = circlesRGBValues[getRandomNumber(circlesRGBValues.length)];   //Saving 1 of the circles as correct correct
-    document.getElementById("guess").innerHTML = "Colour: " + correct;
+    correctCircle = circlesRGBValues[getRandomNumber(circlesRGBValues.length)];   //Saving 1 of the circles as correctCircle correctCircle
+    document.getElementById("guess").innerHTML = "Colour: " + correctCircle;
     document.getElementById("result").innerHTML = "";
 }//O(N)
 
@@ -53,21 +53,21 @@ function vanishCircle(pickedCircleIndex) {
     document.getElementById("result").innerHTML = "WRONG";
 }//O(1)
 
-function correctColouring(run, pickedCircleIndex) {
+function correctCircleColouring(run, pickedCircleIndex) {
     circlesRGBValues[run] = circlesRGBValues[pickedCircleIndex];
     circles[run].style.background = circlesRGBValues[pickedCircleIndex];
     circles[run].style.boxShadow = "5px 5px 105px 5px #ff8c00";
 }//O(1)
 
 function pickCircle(pickedCircleIndex) {
-    if (correct != circlesRGBValues[pickedCircleIndex]) {               //Picked the incorrect circle
+    if (correctCircle != circlesRGBValues[pickedCircleIndex]) {               //Picked the incorrectCircle circle
         vanishCircle(pickedCircleIndex);
         return;
     }
-    for (let run = 0; run < colours.length; run -= -1) {                //Colouring incorrect circles like correct circle
+    for (let run = 0; run < colours.length; run -= -1) {                //Colouring incorrectCircle circles like correctCircle circle
         if (pickedCircleIndex == run)
             continue;
-        correctColouring(run, pickedCircleIndex);
+        correctCircleColouring(run, pickedCircleIndex);
     }
     document.getElementById("result").innerHTML = "CORRECT";            //Writing the user a "you win" message
 }//O(N)
